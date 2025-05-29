6e117eb4a8d10a7084ebe8013402e94c0ed41e9a
icode <github@help.cm>
2025-05-29 17:11:56 +0800

    init

---
+++ .vitepress/config.ts
@@ -0,0 +1,57 @@
+import { defineConfig } from 'vitepress'
+import { getPosts } from './theme/serverUtils'
+
+//每页的文章数量
+const pageSize = 10
+
+const isProd = process.env.NODE_ENV === 'production'
+
+export default defineConfig({
+    title: 'icode',
+    base: '/',
+    cacheDir: './node_modules/vitepress_cache',
+    description: 'vitepress,blog,blog-theme',
+    ignoreDeadLinks: true,
+    themeConfig: {
+        posts: await getPosts(pageSize),
+        website: 'https://blog.icode.one', 
+        comment: {
+            repo: 'icode2010/blog',
+            repoId: 'R_kgDOOyhM_A',
+            categoryId: 'DIC_kwDOOyhM_M4CqudP'
+        },
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Category', link: '/pages/category' },
+            { text: 'Archives', link: '/pages/archives' },
+            { text: 'Tags', link: '/pages/tags' },
+            { text: 'About', link: '/pages/about' }
+        ],
+        search: {
+            provider: 'local'
+        },
+        //outline:[2,3],
+        outline: {
+            label: '文章摘要'
+        },
+        socialLinks: [{ icon: 'github', link: 'https://github.com/icode2010/blog' }]
+    } as any,
+
+    srcExclude: isProd
+        ? [
+              '**/trash/**/*.md', // 排除所有 trash 目录
+              '**/draft/**/*.md', // 递归排除子目录
+              '**/private-notes/*.md', // 排除特定文件
+              'README.md'
+          ]
+        : ['README.md'],
+    vite: {
+        //build: { minify: false }
+        server: { port: 5000 }
+    }
+    /*
+      optimizeDeps: {
+          keepNames: true
+      }
+      */
+})
---
+++ pages/about.md
@@ -0,0 +1,19 @@
+---
+page: true
+title: About
+description: About me
+aside: false
+comment: true
+---
+
+## 关于我
+
+---
+
+- 某四线城市谋生的小程序员
+- 偶尔出门,经常开车
+- 偶尔写博客,大部分时间都在浪费人生
+- 主攻.NET技术,也懂一些linux相关的及其他大部分语言的Hello World
+- 编程的路子未必走多远,只要没转行,依然会写代码
+---
+
